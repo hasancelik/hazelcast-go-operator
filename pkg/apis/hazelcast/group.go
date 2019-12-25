75b65160d38c6c3044804f37c7d2c39a236742d4
hasancelik <hasan@hazelcast.com>
2019-12-25 11:13:09 +0300

    added Hazelcast resource and use all namespaces

---
+++ pkg/apis/hazelcast/group.go
@@ -0,0 +1,6 @@
+// Package hazelcast contains hazelcast API versions.
+//
+// This file ensures Go source parsers acknowledge the hazelcast package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package hazelcast
---
+++ pkg/apis/hazelcast/v1alpha1/hazelcast_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HazelcastSpec defines the desired state of Hazelcast
+type HazelcastSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// HazelcastStatus defines the observed state of Hazelcast
+type HazelcastStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Hazelcast is the Schema for the hazelcasts API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=hazelcasts,scope=Namespaced
+type Hazelcast struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HazelcastSpec   `json:"spec,omitempty"`
+	Status HazelcastStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// HazelcastList contains a list of Hazelcast
+type HazelcastList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Hazelcast `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Hazelcast{}, &HazelcastList{})
+}
